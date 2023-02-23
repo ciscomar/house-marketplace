3501d5098dc5ce91c71cffc57e26d09cde3c8835
ciscomar <cisco.morales.27@gmail.com>
2023-02-23 16:06:11 -0600

    pages, routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,29 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Explore from './pages/Explore'
+import ForgotPassword  from './pages/ForgotPassword'
+import Offers from './pages/Offers'
+import Profile from './pages/Profile'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
 
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
+      <Router>
+        <Routes>
+          <Route path='/' element= {<Explore/>}></Route>
+          <Route path='/offers' element= {<Offers/>}></Route>
+          <Route path='/profile' element= {<Profile/>}></Route>
+          <Route path='/sign-in' element= {<SignIn/>}></Route>
+          <Route path='/sign-up' element= {<SignUp/>}></Route>
+          <Route path='/forgot-password' element= {<ForgotPassword/>}></Route>
 
+        </Routes>
+
+      </Router>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
