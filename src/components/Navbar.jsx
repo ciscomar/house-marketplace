d1a5928181abe50e9e22e5db2c4fdb024202c236
ciscomar <cisco.morales.27@gmail.com>
2023-02-24 14:24:01 -0600

    signin-out

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -19,15 +19,15 @@ const pathMatchRoute = (route) =>{
         <ul className="navbarListItems">
           <li className="navbarListItem" onClick={()=> navigate('/')}>
             <ExploreIcon fill={pathMatchRoute('/') ? '#2c2c2c' : '#8f8f8f'} width='36px' height='36px'/>
-            <p>Explore</p>
+            <p className={pathMatchRoute('/') ? 'navbarListItemNameActive':'navbarListItemName'}>Explore</p>
           </li>
           <li className="navbarListItem" onClick={()=> navigate('/offers')}>
             <OfferIcon  fill={pathMatchRoute('/offers') ? '#2c2c2c' : '#8f8f8f'} width='36px' height='36px'/>
-            <p>Offer</p>
+            <p className={pathMatchRoute('/') ? 'navbarListItemNameActive':'navbarListItemName'}>Offer</p>
           </li>
           <li className="navbarListItem" onClick={()=> navigate('/profile')}>
             <PersonOutlineIcon  fill={pathMatchRoute('/profile') ? '#2c2c2c' : '#8f8f8f'} width='36px' height='36px'/>
-            <p>Profile</p>
+            <p className={pathMatchRoute('/') ? 'navbarListItemNameActive':'navbarListItemName'}>Profile</p>
           </li>
         </ul>
       </nav>
